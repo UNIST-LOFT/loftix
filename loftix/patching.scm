956de57022dadb23722e5d0d010bd8e98897e464
Nguyễn Gia Phong <cnx@loang.net>
2025-04-17 01:24:23 +0900

    Update E9Patch to 1.0.0-rc10

--- loftix/patching.scm
+++ loftix/patching.scm
@@ -17,42 +17,40 @@
   #:use-module (guix packages))
 
 (define-public e9patch
-  (let ((commit "061f8dd6d48c3a6441d8300e697696bf415683a4")
-        (revision "0"))
-    (package
-      (name "e9patch")
-      (version (git-version "1.0.0-dev" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                     (url "https://github.com/GJDuck/e9patch")
-                     (commit commit)))
-                (sha256
-                 (base32
-                  "0l4bzkdfxhsdsjh2gk2pas4kkw8y5yrsl7hx1hlnhx2q0vp60kv6"))
-                (file-name (git-file-name name version))
-                (patches (search-patches
-                           ;; https://github.com/GJDuck/e9patch/pull/94
-                           "patches/e9patch-zydis-4.1.0.patch"
-                           ;; https://github.com/GJDuck/e9patch/pull/92
-                           "patches/e9patch-check.patch"
-                           ;; https://github.com/GJDuck/e9patch/pull/95
-                           "patches/e9patch-check-intel-format.patch"
-                           ;; https://github.com/GJDuck/e9patch/pull/93
-                           "patches/e9patch-check-mov-imm.patch"
-                           ;; https://github.com/GJDuck/e9patch/pull/97
-                           "patches/e9patch-check-same_op_2.patch"))))
-      (build-system gnu-build-system)
-      (arguments (list #:phases #~(modify-phases %standard-phases
-                                    (delete 'configure))
-                       #:make-flags #~(list (string-append
-                                              "PREFIX=" #$output))))
-      (native-inputs (list markdown xxd))
-      (inputs (list elfutils zycore zydis zlib))
-      (home-page "https://github.com/GJDuck/e9patch")
-      (synopsis "Static binary rewriting tool")
-      (description
-       "E9Patch is a static binary rewriting tool for x86-64 ELF binaries.
+  (package
+    (name "e9patch")
+    (version "1.0.0-rc10")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                   (url "https://github.com/GJDuck/e9patch")
+                   (commit (string-append "v" version))))
+              (sha256
+               (base32
+                "1l2pjxgr2mckpffvj7hf0sjvv3678138afjb0wc3f6c2zrcpspf8"))
+              (file-name (git-file-name name version))
+              (patches (search-patches
+                         ;; https://github.com/GJDuck/e9patch/pull/94
+                         "patches/e9patch-zydis-4.1.0.patch"
+                         ;; https://github.com/GJDuck/e9patch/pull/92
+                         "patches/e9patch-check.patch"
+                         ;; https://github.com/GJDuck/e9patch/pull/95
+                         "patches/e9patch-check-intel-format.patch"
+                         ;; https://github.com/GJDuck/e9patch/pull/93
+                         "patches/e9patch-check-mov-imm.patch"
+                         ;; https://github.com/GJDuck/e9patch/pull/97
+                         "patches/e9patch-check-same_op_2.patch"))))
+    (build-system gnu-build-system)
+    (arguments (list #:phases #~(modify-phases %standard-phases
+                                  (delete 'configure))
+                     #:make-flags #~(list (string-append
+                                            "PREFIX=" #$output))))
+    (native-inputs (list markdown xxd))
+    (inputs (list elfutils zycore zydis zlib))
+    (home-page "https://github.com/GJDuck/e9patch")
+    (synopsis "Static binary rewriting tool")
+    (description
+     "E9Patch is a static binary rewriting tool for x86-64 ELF binaries.
 E9Patch is:
 @itemize
 @item Scalable: E9Patch can reliably rewrite large/complex binaries
@@ -64,5 +62,5 @@ E9Patch is:
 @item Programmable: E9Patch is designed so that it can be easily integrated
       into other projects.
 @end itemize")
-      (license (list license:expat ;src/e9patch/e9loader_*.cpp
-                     license:gpl3+))))) ;rest
+    (license (list license:expat ;src/e9patch/e9loader_*.cpp
+                   license:gpl3+)))) ;rest
