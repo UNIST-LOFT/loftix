c9e4defd549aab04465b2462e453789298f447d9
Nguyễn Gia Phong <cnx@loang.net>
2025-06-16 17:49:32 +0900

    Revert "Package python-apted"
    This reverts commit 83f7af4132a3e9ac0ea47db767bae29b98f2dd0b,
    as python-apted is now included in the official Guix channel.
    
    References: https://codeberg.org/guix/guix/commit/c8218094c47482c16f4cdd1e8092c35dab117418

--- loftix/patching.scm
+++ loftix/patching.scm
@@ -1,6 +1,6 @@
 ;;; Packages for software patching
 ;;;
-;;; SPDX-FileCopyrightText: 2024-2025 Nguyễn Gia Phong
+;;; SPDX-FileCopyrightText: 2024 Nguyễn Gia Phong
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (loftix patching)
@@ -11,7 +11,6 @@
   #:use-module (gnu packages markup)
   #:use-module (gnu packages vim)
   #:use-module (guix build-system gnu)
-  #:use-module (guix build-system python)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
@@ -64,30 +63,3 @@ E9Patch is:
 @end itemize")
     (license (list license:expat ;src/e9patch/e9loader_*.cpp
                    license:gpl3+)))) ;rest
-
-(define-public python-apted
-  (let ((commit "828b3e3f4c053f7d35f0b55b0d5597e8041719ac")
-        (revision "0"))
-    (package
-      (name "python-apted")
-      (version (git-version "1.0.3" revision commit))
-      (home-page "https://github.com/JoaoFelipe/apted")
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference (url home-page)
-                             (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "1vw1sbn41cysmhr4ib58cw3hzs1xjxwb1d8r1yhrqgjk5q6ckjw7"))))
-      (build-system python-build-system)
-      (arguments
-       (list #:phases #~(modify-phases %standard-phases
-                          (replace 'check
-                            (lambda* (#:key tests? #:allow-other-keys)
-                              (when tests?
-                                (invoke "python" "-m" "unittest")))))))
-      (synopsis "APTED algorithm for the Tree Edit Distance")
-      (description "This is a Python implementation of the APTED algorithm,
-which supersedes the RTED algorithm for computing the tree edit distance.")
-      (license license:expat))))
