UNIST-LOFT/loftix

commit 41eb0fa6adc7b9a007dc2438f5cd70ff34de6373

Remove unused import

loftix/bugs.scm
@@ -36,8 +36,7 @@
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix utils)
-  #:use-module (srfi srfi-1))
+  #:use-module (guix utils))
 
 (define-public bux
   (package

commit bb8319f1a2195a476d6073c1f3de2eefff7cf5d5

Re-add SRFI-1 for alist-delete
Fixes: 41eb0fa6adc7 ("Remove unused import")

loftix/bugs.scm
@@ -36,7 +36,8 @@
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module ((srfi srfi-1) #:select (alist-delete)))
 
 (define-public bux
   (package