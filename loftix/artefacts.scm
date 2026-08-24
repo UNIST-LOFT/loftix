d7c07fabad9311f59d8f050b1eaefb1b5b0f4bf2
Nguyễn Gia Phong <cnx@loang.net>
2026-08-24 17:55:28 +0900

    Add relevant taosc eval artefacts from VulnLoc benchmark

--- loftix/artefacts.scm
+++ loftix/artefacts.scm
@@ -47,6 +47,45 @@
       (home-page (package-home-page buggy-package))
       (license (package-license buggy-package)))))
 
+(define-public taoscadh-cve-2017-6965
+  (taosctha binutils-2.27 "CVE-2017-6965"))
+
+(define-public taoscadh-cve-2017-14745
+  (taosctha binutils-2.29 "CVE-2017-14745"))
+
+(define-public taoscadh-cve-2017-15020
+  (taosctha binutils-2.29 "CVE-2017-15020"))
+
+(define-public taoscadh-cve-2017-15025
+  (taosctha binutils-2.29 "CVE-2017-15025"))
+
+(define-public taoscadh-cve-2016-8691
+  (taosctha jasper-static-1.900.3 "CVE-2016-8691"))
+
+(define-public taoscadh-cve-2016-9557
+  (taosctha jasper-static-1.900.19 "CVE-2016-9557"))
+
+(define-public taoscadh-cve-2012-2806
+  (taosctha libjpeg-turbo-static-1.2.0 "CVE-2012-2806"))
+
+(define-public taoscadh-cve-2017-15232
+  (taosctha libjpeg-turbo-static-1.5.2 "CVE-2017-15232"))
+
+(define-public taoscadh-cve-2018-14498
+  (taosctha libjpeg-turbo-static-1.5.3 "CVE-2018-14498"))
+
+(define-public taoscadh-cve-2018-19664
+  (taosctha libjpeg-turbo-static-2.0.1 "CVE-2018-19664"))
+
+(define-public taoscadh-cve-2016-9265
+  (taosctha libming-0.4.7 "CVE-2016-9265"))
+
+(define-public taoscadh-cve-2018-8806
+  (taosctha libming-0.4.8 "CVE-2018-8806"))
+
+(define-public taoscadh-cve-2018-8964
+  (taosctha libming-0.4.8 "CVE-2018-8964"))
+
 (define-public taoscadh-cve-2016-5314
   (taosctha libtiff-static-4.0.6 "CVE-2016-5314"))
 
@@ -79,3 +118,21 @@
 
 (define-public taoscadh-maptools-2633
   (taosctha libtiff-static-4.0.7 "Maptools-2633"))
+
+(define-public taoscadh-cve-2012-5134
+  (taosctha libxml2-2.9.0 "CVE-2012-5134"))
+
+(define-public taoscadh-cve-2016-1839
+  (taosctha libxml2-2.9.3 "CVE-2016-1839"))
+
+(define-public taoscadh-cve-2017-5969
+  (taosctha libxml2-2.9.4 "CVE-2017-5969"))
+
+(define-public taoscadh-cve-2013-7437
+  (taosctha potrace-1.11 "CVE-2013-7437"))
+
+(define-public taoscadh-cve-2017-5974
+  (taosctha zziplib-0.13.62 "CVE-2017-5974"))
+
+(define-public taoscadh-cve-2017-5975
+  (taosctha zziplib-0.13.62 "CVE-2017-5975"))
