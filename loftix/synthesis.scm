d0668633259d8ecff220819392c9d0b05bad93a0
Nguyễn Gia Phong <cnx@loang.net>
2026-08-26 12:07:09 +0900

    Update taosc to 0.0.18

--- loftix/synthesis.scm
+++ loftix/synthesis.scm
@@ -132,7 +132,7 @@ and congruence relations.")
 (define-public taosc
   (package
     (name "taosc")
-    (version "0.0.17")
+    (version "0.0.18")
     (source
      (origin
        (method fossil-fetch)
@@ -140,7 +140,7 @@ and congruence relations.")
              (uri "https://chim.loan/taosc")
              (check-in version)))
        (sha256
-        (base32 "0jnwdk4pc9x15y7fijlc9zg7lpgqgg2rdhysv21i2jqir9alfa1b"))))
+        (base32 "0l85ik3liv9jw5xa8wpiqbwavy4ig1dw7mz7ac4wd5zs9q80l18b"))))
     (build-system gnu-build-system)
     (arguments
      (list
