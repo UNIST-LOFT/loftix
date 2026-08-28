bf8aa976ef3343d1c5b09ce9712f023309835720
Nguyễn Gia Phong <cnx@loang.net>
2026-08-28 12:18:44 +0900

    Revert "Supply binutils, findutils and gcc for e9compile"
    This reverts commits 7ea4c0487d9d2354d99ade2be31140ef92ebf281
    and fcb67e1d53a190b6754aaf893a132a96135929c4.

--- loftix/artefacts.scm
+++ loftix/artefacts.scm
@@ -5,7 +5,6 @@
 
 (define-module (loftix artefacts)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages gcc)
   #:use-module (guix build-system trivial)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -39,8 +38,7 @@
                          (string-append #$buggy-package #$bin)
                          (simple-format #f #$args "@@")))))
       (inputs (list buggy-package))
-      (native-inputs
-       (list binutils bux coreutils diffutils findutils gcc grep sed taosc))
+      (native-inputs (list bux coreutils diffutils grep sed taosc))
       (synopsis (simple-format #f "~a@@~a with ~a patched by taosc"
                   (package-name buggy-package)
                   (package-version buggy-package)
