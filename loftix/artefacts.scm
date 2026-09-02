bb8cc6d3cd9e5f42fb12976f71ca005ba43e5c69
Nguyễn Gia Phong <cnx@loang.net>
2026-09-02 13:40:08 +0900

    Include bug ID in package name
    Cuirass does not support searching by version.

--- loftix/artefacts.scm
+++ loftix/artefacts.scm
@@ -13,15 +13,15 @@
   #:use-module (loftix synthesis)
   #:export (taosctha))
 
-(define (taosctha buggy-package bug-id)
+(define (taosctha bug-id buggy-package)
   (let* ((poc (cadr (search-bug %buggy-packages bug-id)))
          (bin (string-append "/bin/" (car poc)))
          (args (cadr poc))
          (poc-dir (string-append "share/bux/" (dirname (caddr poc))))
          (timeout "67"))
     (package
-      (name "taoscadh")
-      (version bug-id)
+      (name (string-append "taoscadh-" (string-downcase bug-id)))
+      (version (package-version taosc))
       (source #f)
       (build-system trivial-build-system)
       (arguments
@@ -48,133 +48,133 @@
       (license (package-license buggy-package)))))
 
 (define-public taoscadh-cve-2017-6965
-  (taosctha binutils-2.27 "CVE-2017-6965"))
+  (taosctha "CVE-2017-6965" binutils-2.27))
 
 (define-public taoscadh-cve-2017-6966
-  (taosctha binutils-2.27 "CVE-2017-6966"))
+  (taosctha "CVE-2017-6966" binutils-2.27))
 
 (define-public taoscadh-cve-2017-14745
-  (taosctha binutils-2.29 "CVE-2017-14745"))
+  (taosctha "CVE-2017-14745" binutils-2.29))
 
 (define-public taoscadh-cve-2017-14939
-  (taosctha binutils-2.29 "CVE-2017-14939"))
+  (taosctha "CVE-2017-14939" binutils-2.29))
 
 (define-public taoscadh-cve-2017-14940
-  (taosctha binutils-2.29 "CVE-2017-14940"))
+  (taosctha "CVE-2017-14940" binutils-2.29))
 
 (define-public taoscadh-cve-2017-15020
-  (taosctha binutils-2.29 "CVE-2017-15020"))
+  (taosctha "CVE-2017-15020" binutils-2.29))
 
 (define-public taoscadh-cve-2017-15025
-  (taosctha binutils-2.29 "CVE-2017-15025"))
+  (taosctha "CVE-2017-15025" binutils-2.29))
 
 (define-public taoscadh-cve-2017-15938
-  (taosctha binutils-2.29 "CVE-2017-15938"))
+  (taosctha "CVE-2017-15938" binutils-2.29))
 
 (define-public taoscadh-cve-2018-10372
-  (taosctha binutils-2.30 "CVE-2018-10372"))
+  (taosctha "CVE-2018-10372" binutils-2.30))
 
 (define-public taoscadh-cve-2019-9077
-  (taosctha binutils-2.32 "CVE-2019-9077"))
+  (taosctha "CVE-2019-9077" binutils-2.32))
 
 (define-public taoscadh-cve-2016-8691
-  (taosctha jasper-static-1.900.3 "CVE-2016-8691"))
+  (taosctha "CVE-2016-8691" jasper-static-1.900.3))
 
 (define-public taoscadh-cve-2016-9557
-  (taosctha jasper-static-1.900.19 "CVE-2016-9557"))
+  (taosctha "CVE-2016-9557" jasper-static-1.900.19))
 
 (define-public taoscadh-cve-2016-9560
-  (taosctha jasper-static-1.900.19 "CVE-2016-9560"))
+  (taosctha "CVE-2016-9560" jasper-static-1.900.19))
 
 (define-public taoscadh-cve-2012-2806
-  (taosctha libjpeg-turbo-static-1.2.0 "CVE-2012-2806"))
+  (taosctha "CVE-2012-2806" libjpeg-turbo-static-1.2.0))
 
 (define-public taoscadh-cve-2017-15232
-  (taosctha libjpeg-turbo-static-1.5.2 "CVE-2017-15232"))
+  (taosctha "CVE-2017-15232" libjpeg-turbo-static-1.5.2))
 
 (define-public taoscadh-cve-2018-14498
-  (taosctha libjpeg-turbo-static-1.5.3 "CVE-2018-14498"))
+  (taosctha "CVE-2018-14498" libjpeg-turbo-static-1.5.3))
 
 (define-public taoscadh-cve-2018-19664
-  (taosctha libjpeg-turbo-static-2.0.1 "CVE-2018-19664"))
+  (taosctha "CVE-2018-19664" libjpeg-turbo-static-2.0.1))
 
 (define-public taoscadh-cve-2016-9265
-  (taosctha libming-static-0.4.7 "CVE-2016-9265"))
+  (taosctha "CVE-2016-9265" libming-static-0.4.7))
 
 (define-public taoscadh-cve-2018-8806
-  (taosctha libming-static-0.4.8 "CVE-2018-8806"))
+  (taosctha "CVE-2018-8806" libming-static-0.4.8))
 
 (define-public taoscadh-cve-2018-8964
-  (taosctha libming-static-0.4.8 "CVE-2018-8964"))
+  (taosctha "CVE-2018-8964" libming-static-0.4.8))
 
 (define-public taoscadh-cve-2014-8128
-  (taosctha libtiff-static-4.0.3 "CVE-2014-8128"))
+  (taosctha "CVE-2014-8128" libtiff-static-4.0.3))
 
 (define-public taoscadh-cve-2016-3186
-  (taosctha libtiff-static-4.0.6 "CVE-2016-3186"))
+  (taosctha "CVE-2016-3186" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-3623
-  (taosctha libtiff-static-4.0.6 "CVE-2016-3623"))
+  (taosctha "CVE-2016-3623" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-5314
-  (taosctha libtiff-static-4.0.6 "CVE-2016-5314"))
+  (taosctha "CVE-2016-5314" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-5321
-  (taosctha libtiff-static-4.0.6 "CVE-2016-5321"))
+  (taosctha "CVE-2016-5321" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-9273
-  (taosctha libtiff-static-4.0.6 "CVE-2016-9273"))
+  (taosctha "CVE-2016-9273" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-9532
-  (taosctha libtiff-static-4.0.6 "CVE-2016-9532"))
+  (taosctha "CVE-2016-9532" libtiff-static-4.0.6))
 
 (define-public taoscadh-cve-2016-10092
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10092"))
+  (taosctha "CVE-2016-10092" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10093
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10093"))
+  (taosctha "CVE-2016-10093" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10094
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10094"))
+  (taosctha "CVE-2016-10094" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10266
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10266"))
+  (taosctha "CVE-2016-10266" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10267
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10267"))
+  (taosctha "CVE-2016-10267" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10268
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10268"))
+  (taosctha "CVE-2016-10268" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10271
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10271"))
+  (taosctha "CVE-2016-10271" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2016-10272
-  (taosctha libtiff-static-4.0.7 "CVE-2016-10272"))
+  (taosctha "CVE-2016-10272" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2017-5225
-  (taosctha libtiff-static-4.0.7 "CVE-2017-5225"))
+  (taosctha "CVE-2017-5225" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2017-7595
-  (taosctha libtiff-static-4.0.7 "CVE-2017-7595"))
+  (taosctha "CVE-2017-7595" libtiff-static-4.0.7))
 
 (define-public taoscadh-maptools-2633
-  (taosctha libtiff-static-4.0.7 "Maptools-2633"))
+  (taosctha "Maptools-2633" libtiff-static-4.0.7))
 
 (define-public taoscadh-cve-2012-5134
-  (taosctha libxml2-static-2.9.0 "CVE-2012-5134"))
+  (taosctha "CVE-2012-5134" libxml2-static-2.9.0))
 
 (define-public taoscadh-cve-2016-1839
-  (taosctha libxml2-static-2.9.3 "CVE-2016-1839"))
+  (taosctha "CVE-2016-1839" libxml2-static-2.9.3))
 
 (define-public taoscadh-cve-2017-5969
-  (taosctha libxml2-static-2.9.4 "CVE-2017-5969"))
+  (taosctha "CVE-2017-5969" libxml2-static-2.9.4))
 
 (define-public taoscadh-cve-2013-7437
-  (taosctha potrace-1.11 "CVE-2013-7437"))
+  (taosctha "CVE-2013-7437" potrace-1.11))
 
 (define-public taoscadh-cve-2017-5974
-  (taosctha zziplib-static-0.13.62 "CVE-2017-5974"))
+  (taosctha "CVE-2017-5974" zziplib-static-0.13.62))
 
 (define-public taoscadh-cve-2017-5975
-  (taosctha zziplib-static-0.13.62 "CVE-2017-5975"))
+  (taosctha "CVE-2017-5975" zziplib-static-0.13.62))
