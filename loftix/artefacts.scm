5a9ef01863e3fe77b39a004e7e4a50d99bd52eb0
Nguyễn Gia Phong <cnx@loang.net>
2026-09-03 13:54:52 +0900

    Fix JasPer version for CVE-2016-9387

--- loftix/artefacts.scm
+++ loftix/artefacts.scm
@@ -77,7 +77,7 @@
   (taosctha "CVE-2016-8691" jasper-static-1.900.3))
 
 (define-public taoscadh-cve-2016-9387
-  (taosctha "CVE-2016-9387" jasper-static-1.900.19))
+  (taosctha "CVE-2016-9387" jasper-static-1.900.5))
 
 (define-public taoscadh-cve-2016-9557
   (taosctha "CVE-2016-9557" jasper-static-1.900.19))
