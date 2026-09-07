c089b841c95a5dd4ceb14d6bbad085935c604047
Nguyễn Gia Phong <cnx@loang.net>
2026-09-07 10:35:49 +0900

    Add Guix channel and manifest files for KTL
    To run taosc on the subjects to be evaluated,
    
    guix time-machine -C test/channel.scm -- build -m test/taosc.scm

---
+++ test/channel.scm
@@ -0,0 +1,10 @@
+(cons* (channel
+         (name 'loftix)
+         (url "https://github.com/UNIST-LOFT/loftix")
+         (commit "deb61b6abd30bcc869171750bd83f05b41f0984f")
+         (introduction
+          (make-channel-introduction
+           "1b5437ce217590545f7a7319a5c62b6300aee6c4"
+           (openpgp-fingerprint
+            "838A FE0D 55DC 074E 360F  943A 84B6 9CE6 F3F6 B767"))))
+       %default-channels)
